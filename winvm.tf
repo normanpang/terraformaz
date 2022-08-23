8faa9087056a0528721095e2592adf1728f3107c
Norman Pang <normpang@gmail.com>
2022-08-23 08:37:27 +0000

    3rd commit

---
+++ winvm.tf
@@ -0,0 +1,37 @@
+## <https://www.terraform.io/docs/providers/azurerm/r/network_interface.html>
+resource "azurerm_network_interface" "winvm" {
+  name                = "winvm-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "pit-lab-winvm-ifc"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+## <https://www.terraform.io/docs/providers/azurerm/r/windows_virtual_machine.html>
+resource "azurerm_windows_virtual_machine" "winvm" {
+  name                = "winvm-machine"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+##  availability_set_id = azurerm_availability_set.DemoAset.id
+  network_interface_ids = [
+    azurerm_network_interface.winvm.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
